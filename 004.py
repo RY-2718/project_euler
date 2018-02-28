a6008b85a09aa01ee69e1149a2216760db79c0d7
RY_2718(Ryo Yoshitani) <ry.3607x5531@gmail.com>
2018-02-28 14:27:57 +0900

    :sparkles: Add: Problem 4

--- 004.py
+++ 004.py
@@ -1,4 +1,15 @@
-def makePalindromicNumber(string):
-    return int(string + string[::-1])
+import math
+import sys
 
-print(makePalindromicNumber("123"))
+def makePalindromicNumber(input):
+    input = str(input)
+    return int(input + input[::-1])
+
+origin = 999
+while origin >= 100:
+    num = makePalindromicNumber(origin)
+    for i in range(100, int(math.sqrt(num))):
+        if num % i == 0 and num / i >= 100 and num / i <= 999:
+            print(num, i, num / i)
+            sys.exit(0)
+    origin -= 1
